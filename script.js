509eb7ef3931eb998cf6647e9c6a16c77e41c680
varungarg2796 <varungarg2796@hotmail.com>
2017-03-30 22:18:54 +0530

    Added the search icon

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //Some javascript will come here
+;(function(){
+	
+	'use strict';
+
+	var expandSearch = {
+		init: function(){
+
+			var _this = this,
+			_searchContainers = document.querySelectorAll('.expandSearch');
+
+			for( var _index in _searchContainers ){
+				if( typeof _searchContainers[ _index ] === 'object' ){
+					_this.searchFunctions( _searchContainers[ _index ] );
+				}
+			}
+
+		},
+
+		searchFunctions: function( _thisSearch ){
+				
+			var _nodes = _thisSearch.childNodes;
+
+			//a click
+			_nodes[3].addEventListener('click',function(e){
+
+				if( _thisSearch.attributes.class.value.indexOf("showSearch") > -1 ){
+					_thisSearch.attributes.class.value = 'expandSearch';
+				}
+				else{
+					_thisSearch.attributes.class.value = 'expandSearch showSearch';
+				}
+
+				if( !e.preventDefault() ){ e.returnValue = false; }
+			});
+
+		}
+
+	};
+
+
+	//execute
+	expandSearch.init();
+
+}());
