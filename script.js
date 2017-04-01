cfc66caafaa8492c17ee0e3bb29037ecc45708e2
varungarg2796 <varungarg2796@hotmail.com>
2017-04-02 01:08:25 +0530

    Functionality complete

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
- 
+var obj;
+var results;
+var desc;
+var link;
+
+
 $("#butt").click(function() {
 	 $.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $('#search').val() + '&limit=5&namespace=0&format=json', function( data ) {
 	  	console.log(data);
-
+	  	obj=data;
+	  	results=obj[1];
+	  	desc=obj[2];
+	  	link=obj[3];
+	  	appendResults(obj,results,desc,link);
 
 	      //make tags the correct array
 	      //tags = data[1];
@@ -14,6 +23,18 @@ $("#butt").click(function() {
 	}, "jsonp" );
    
  })
+
+function appendResults(obj,results,desc,link)
+{
+	document.getElementById("divID").innerHTML = "";
+	for (var i=0; i<5; i++) 
+	{
+		var div = document.getElementById('divID');
+		console.log(results[i]);
+		div.innerHTML = div.innerHTML + 
+		'<a target="_blank" href='+link[i]+'><div class="infoDiv">'+results[i]+'<br>'+ desc[i] +        '</div></a>';
+	}
+}
 
 
 
